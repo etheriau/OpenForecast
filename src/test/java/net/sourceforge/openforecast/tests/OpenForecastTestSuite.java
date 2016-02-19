d530489fefb8df9d8c4c2aa1543cd0c91491f59f
Eric Y. Theriault <eric@myscenicdrives.com>
2016-02-19 09:14:37 -0800

    Fix compilation

--- src/test/java/net/sourceforge/openforecast/tests/OpenForecastTestSuite.java
+++ src/test/java/net/sourceforge/openforecast/tests/OpenForecastTestSuite.java
@@ -20,6 +20,7 @@
 package net.sourceforge.openforecast.tests;
 
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 
@@ -35,23 +36,23 @@ public class OpenForecastTestSuite
         try
             {
                 // Test models and core classes
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.BasicTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.DataSetTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.SimpleExponentialSmoothingTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.DoubleExponentialSmoothingTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.TripleExponentialSmoothingTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.MovingAverageTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.MultipleLinearRegressionTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.PolynomialRegressionTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.WeightedMovingAverageTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.BasicTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.DataSetTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.SimpleExponentialSmoothingTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.DoubleExponentialSmoothingTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.TripleExponentialSmoothingTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.MovingAverageTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.MultipleLinearRegressionTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.PolynomialRegressionTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.WeightedMovingAverageTest" ) );
 
                 // Test builder(s)
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.CSVBuilderTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.TimeSeriesBuilderTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.CSVBuilderTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.TimeSeriesBuilderTest" ) );
                 
                 // Test outputter(s)
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.DelimitedTextOutputterTest" ) );
-                suite.addTestSuite( Class.forName( "net.sourceforge.openforecast.tests.TimeSeriesOutputterTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.DelimitedTextOutputterTest" ) );
+                suite.addTestSuite( (Class<TestCase>) Class.forName( "net.sourceforge.openforecast.tests.TimeSeriesOutputterTest" ) );
             }
         catch ( ClassNotFoundException exCNF )
             {
